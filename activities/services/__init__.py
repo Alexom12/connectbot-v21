218606d8fafe108e07ed595804d7ca4588e7838a
ConnectBot Developer <dev@connectbot.local>
2025-10-15 22:51:53 +0300

    fix(activities): export ActivityManager in services package

--- activities/services/__init__.py
+++ activities/services/__init__.py
@@ -6,12 +6,13 @@ from .anonymous_coffee_service import anonymous_coffee_service
 from .java_matching_service import java_matching_service
 from .preference_service import preference_service
 from .redis_service import activity_redis_service
-from .activity_manager import activity_manager
+from .activity_manager import ActivityManager, activity_manager
 
 __all__ = [
     'anonymous_coffee_service',
     'java_matching_service', 
     'preference_service',
     'activity_redis_service',
+    'ActivityManager',
     'activity_manager'
 ]
